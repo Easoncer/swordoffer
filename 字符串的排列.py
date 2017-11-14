f024f41e19a26bd5b9cb32f162a839eebeaf0cee
llc <574014651@qq.com>
2017-11-14 12:30:07 +0800

    全排列-string

--- 字符串的排列.py
+++ 字符串的排列.py
@@ -1,32 +1,33 @@
 # aha algrorithm
 
 
-def dfs(step):
-	if step==n:
+def dfs(step,ts):
+	if step==len(a):
 		result.append(''.join(a))
 		return
 
-	for i in range(len(ts)):
+	for i in range(len(a)):
 		if book[i]==0:
 			a[step]=ts[i]
 			book[i]=1
-			dfs(step+1)
+			dfs(step+1,ts)
 			book[i]=0
 	return 
 
 if __name__=="__main__":
 	book=[0 for i in range(0,10)]
 	n=4
-	ss="aad"
+	ss="aab"
 
 	ts=list(ss)
 	result=[]
-	ts.sort()
 	n=len(ts)
-	book=[0 for i in range(n)]
-	a=[0 for i in range(n)]
-	dfs(0)
-	print list(set(result))
+	book=[0 for i in range(len(ts))]
+	a=[0 for i in range(len(ts))]
+	dfs(0,ts)
+	a=list(set(result))
+	a.sort()
+	print a
 	# a=list(set(list(ss).sort()))
 	# print a
 	# dfs(1)
