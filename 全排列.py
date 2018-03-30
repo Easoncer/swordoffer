cb28c7145b88c6b90907cb20e63e6649194271d9
llc <574014651@qq.com>
2018-03-30 17:05:31 +0800

    全排列

---
+++ 全排列.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+'''
+
+@author: liangchi
+
+@contact: bnu_llc@163.com
+
+@software: pycharm
+
+@file: 全排列.py
+
+@time: 2018/3/30 下午4:14
+
+'''
+
+if __name__ == '__main__':
+
