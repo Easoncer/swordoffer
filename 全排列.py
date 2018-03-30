ddff4a283fc5c3ea9432428a91c6d4ab1f580e1f
llc <574014651@qq.com>
2018-03-31 01:41:48 +0800

    iter_tree

--- 全排列.py
+++ 全排列.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # encoding: utf-8
 
 '''
@@ -15,6 +13,26 @@
 @time: 2018/3/30 下午4:14
 
 '''
+def dfs(step, mark, number):
+    if step == n+1:
+        print number[1:]
+        return
+
+    for i in range(1, n+1):
+        if mark[i] == 0:
+            number[step] = i
+            mark[i] = 1
+            dfs(step+1, mark, number)
+            mark[i] = 0
+    return
+
+def Permutations(n):
+    # all the numbers
+    number = range(0,n+1)
+    # whether used
+    mark = [0 for i in range(0,n+1)]
+    dfs(1,mark, number)
 
 if __name__ == '__main__':
-
+    n = 5
+    Permutations(n)
